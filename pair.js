4ba4b6525e0de8548af5ec358162d06cf3a2f443
BLACKVENOM499 <blackvenomx499@gmail.com>
2026-03-30 21:36:34 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -24,7 +24,7 @@ const {
     prepareWAMessageMedia,
     generateWAMessageFromContent,
     S_WHATSAPP_NET
-} = require('@whiskeysockets/baileys');
+} = require('baileyz');
 
 // ==================== CONFIGURATION ====================
 const config = {
